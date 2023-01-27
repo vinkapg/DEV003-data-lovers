9785d5321b26c0b1156ec9c836070a068fe230ca
Pgfeliu <pamela.gfeliu@gmail.com>
2023-01-27 12:13:44 -0300

    cambios 27-10-23

--- src/data.js
+++ src/data.js
@@ -1,4 +1,33 @@
-export const personajes = (data,house) => {
-  const datosCharacters = data.characters.filter(characters => (characters.house === house));
-  return datosCharacters
+export const genero = (data,gender) => {
+  const datosGenero = data.characters.filter(characters => (characters.gender === gender));
+  return datosGenero
 }
+export function sortMe(data) {
+  const dataOrdenadaAZ = data.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if(nameA < nameB){
+      return -1;
+    }
+    if(nameA > nameB){
+      return 1;
+    }
+    return 0;
+  });
+  return dataOrdenadaAZ
+}
+export function sortMe2(data) {
+  const dataOrdenadaZA = data.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+    if(nameA < nameB){
+      return 1;
+    }
+    if(nameA > nameB){
+      return -1;
+    }
+    return 0;
+  });
+  return dataOrdenadaZA
+}
+
